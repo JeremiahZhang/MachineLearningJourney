697c162666dcb8914fec072b61acbbf8f10af45f
JeremiahZhang <zhangleisuda@gmail.com>
2016-04-23 16:43:52 +0800

    words and occurence ordered successful

--- python ml/count_words.py
+++ python ml/count_words.py
@@ -6,17 +6,28 @@ def count_words(s, n):
 
     # TODO: Count the number of occurences of each word in s
 
-    # TODO: Sort the occurences in descending order (alphabetically in case of ties)
+    words_list = s.split()
+    unique_word_set = set(words_list)
+    freq = {}
+    for word in unique_word_set:
+        freq[word] = words_list.count(word)
 
-    # TODO: Return the top n words as a list of tuples (<word>, <count>)
+    # TODO: Sort the occurences in descending order (alphabetically in case of ties)
+    words_freq_pair = freq.items()
+    words_freq_pair.sort(key=lambda pair: (-pair[1], pair[0]))
+    # select the first n words_and freqruence pair
+    top_n = words_freq_pair[:n]
     return top_n
 
 
 def test_run():
     """Test count_words() with some inputs."""
-    print count_words("cat bat mat cat bat cat", 3)
+    print count_words("cat bat mat cat bat cat zion", 3)
     print count_words("betty bought a bit of butter but the butter was bitter", 3)
 
 
 if __name__ == '__main__':
     test_run()
+
+# reference thanks
+'''http://codereview.stackexchange.com/questions/118914/word-frequency-counter'''
--- python ml/sort_phrase.py
+++ python ml/sort_phrase.py
@@ -14,10 +14,7 @@ def count_words(s, n):
     for i in range(n):
         top_n.append((dicts_word_by_occurence[i], dicts[dicts_word_by_occurence[i]]))
 
-    top_n = sorted(top_n, key=lambda x: x[0])
-
-    def same_order(item):
-
+    top_n = sorted(top_n, key=lambda tup: (-tup[1], tup[0]))
 
     # TODO: Return the top n words as a list of tuples (<word>, <count>)
 
@@ -25,36 +22,8 @@ def count_words(s, n):
 
 def test_run():
     """Test count_words() with some inputs."""
-    print count_words("cat bat mat cat bat cat zion", 4)
+    print count_words("cat bat mat cat bat cat zion", 3)
     print count_words("betty bought a bit of butter but the butter was bitter", 3)
 
 if __name__ == '__main__':
-        words = "cat bat mat cat bat cat"
-    words_list = words.split()
-    # sorted_words = sorted(words_list, key=str.lower)
-
-    # list
-    # pairs = []
-    # for w in words_list:
-    #     pairs.append((w, words_list.count(w)))
-
-    # diff_words_counts = list(set(pairs))
-    # print diff_words_counts
-
-    # dictionary
-    dicts = {}
-    for w in words_list:
-        dicts.update({w: words_list.count(w)})
-    print dicts
-    # order words by it's occurence
-    dicts_key_by_number = sorted(dicts, key=dicts.__getitem__, reverse=True)
-     # occurence
-    print type(dicts[dicts_key_by_number[0]])
-    # key words
-    print type(dicts_key_by_number[0])
-    top_n = []
-    for i in range(2):
-        top_n.append((dicts_key_by_number[i], dicts[dicts_key_by_number[i]]))
-    print top_n
-
     test_run()
