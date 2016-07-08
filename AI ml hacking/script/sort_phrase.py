d16bfee5008caa0456a55db5ca489b627d815e0d
JeremiahZhang <zhangleisuda@gmail.com>
2016-07-08 15:47:58 +0800

    udacity intro test

---
+++ AI ml hacking/script/sort_phrase.py
@@ -0,0 +1,26 @@
+"""Count words."""
+
+def count_words(s, n):
+    """Return the n most frequently occuring words in s."""
+    # TODO: Count the number of occurences of each word in s
+    words_in_s = s.split()
+    dicts = {}
+    for w in words_in_s:
+        dicts.update({w: words_in_s.count(w)})
+    # TODO: Sort the occurences in descending order (alphabetically in case of ties)
+    def sameoccurence():
+        
+    dicts_word_by_occurence = sorted(dicts, key=dicts.__getitem__, reverse=True)
+    top_n = {}
+    for i in range(n):
+        top_n.append((dicts_word_by_occurence[i], dicts[dicts_word_by_occurence[i]]))
+    # TODO: Return the top n words as a list of tuples (<word>, <count>)
+    return top_n
+    
+def test_run():
+    """Test count_words() with some inputs."""
+    print count_words("cat bat mat cat bat cat", 3)
+    print count_words("betty bought a bit of butter but the butter was bitter", 3)
+
+if __name__ == '__main__':
+    test_run()
